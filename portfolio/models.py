8f7a31ef7896de91089e1f5f9a15abc1b548429a
Shaniya Khan <shaniaakhan21@gmail.com>
2024-11-19 00:07:48 +0400

    fix media

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.utils.text import slugify
+
+def get_image_filename(instance, filename):
+    # Get the base file name (without extension)
+    base_filename, extension = os.path.splitext(filename)
+    # Create a slug of the original filename to avoid any conflicts
+    slug = slugify(base_filename)
+    # Return the file path with the same filename
+    return os.path.join('posts/', f'{slug}{extension}')
 
 class Post(models.Model):
     author = models.CharField(max_length=100)
