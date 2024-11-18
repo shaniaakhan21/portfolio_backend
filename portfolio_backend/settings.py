b710708d9a28272ad1b0e756a6ed3052d3d86c40
Shaniya Khan <shaniaakhan21@gmail.com>
2024-11-18 23:21:00 +0400

    db error

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -97,11 +97,6 @@ DATABASES = {
     }
 }
 
-
-# Use dj-database-url for Heroku
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
